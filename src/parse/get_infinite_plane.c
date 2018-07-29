4e29c2c57fee0f1b4a0a55ea1a28b1aff79cc5c8
abutok <abutok@student.unit.ua>
2018-07-29 18:02:54 +0300

    Fixed plane lil parse bug.

--- src/parse/get_infinite_plane.c
+++ src/parse/get_infinite_plane.c
@@ -23,8 +23,8 @@ void	get_infinite_plane(t_figure *figure, JSON_Object *obj)
 		ft_putendl_fd("Plane normale not found. Default applied.",
 					  STDERR_FILENO);
 	if (json_object_has_value_of_type(obj, "point", JSONArray))
-		figure->vector1 = get_vector(json_object_get_array(obj, "normale"),
-									 figure->vector1);
+		figure->vector2 = get_vector(json_object_get_array(obj, "point"),
+									 figure->vector2);
 	else
 		ft_putendl_fd("Plane point not found. Default applied.",
 					  STDERR_FILENO);
