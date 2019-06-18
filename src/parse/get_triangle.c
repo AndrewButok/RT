b785c58e39d088a030d7907fa1b49fbee79d23a0
abutok <andrewbutok@ukr.net>
2019-06-18 15:24:12 +0300

    Triangle parse fixed

--- src/parse/get_triangle.c
+++ src/parse/get_triangle.c
@@ -6,7 +6,7 @@
 /*   By: abutok <abutok@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/16 10:12:00 by abutok            #+#    #+#             */
-/*   Updated: 2019/06/17 16:13:24 by abutok           ###   ########.fr       */
+/*   Updated: 2019/06/18 15:21:03 by abutok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,10 @@ bool					get_triangle(t_figure *figure, JSON_Object *obj,
 		json_array_get_count(point_array =
 		json_object_get_array(obj, "points")) != 3)
 		return (false);
-	get_vector(json_array_get_array(point_array, 0), &figure->vector1);
-	get_vector(json_array_get_array(point_array, 1), &figure->vector2);
-	get_vector(json_array_get_array(point_array, 2), &figure->vector3);
+	if (!(get_vector(json_array_get_array(point_array, 0), &figure->vector1) &&
+		get_vector(json_array_get_array(point_array, 1), &figure->vector2) &&
+		get_vector(json_array_get_array(point_array, 2), &figure->vector3)))
+		return (false); 
 	if (!get_figure_params(figure, obj, pf))
 		return (false);
 	return (true);
